5b89a269e5435b4d18812d293dbe743eba3582a0
Amine <amineaithamma2004@gmail.com>
2023-07-07 22:43:57 +0100

    Test 1

--- testmony_1.py
+++ testmony_1.py
@@ -4,11 +4,11 @@ def download_profil_pic(username):
     try:
         loader = instaloader.Instaloader()
         loader.download_profil(username, profile_pic_only=True)
-        print("Profil picture downloaded")
+        print("Profil picture downloaded. ")
 
     except instaloader.exceptions.ProfileNotExistsException:
-        print("The pic doesn't exist")
+        print("The pic doesn't exist. ")
 
-username = input("Enter the name")
+username = input("Enter the name ? ")
 
 download_profil_pic(username)
